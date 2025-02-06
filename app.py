17fbf53f1ae2d2fddee588e01c21a600afe401d1
Ankit Singh <ankitsingh18092002@gmail.com>
2025-02-06 14:19:36 +0530

    Added memory in llm

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import gradio as gr
 from components.llm import LLM
-    
+
 def process_llm(message, history):
     history = history or []
-    llm = LLM(query=message)
+
+    llm = LLM(query=message, history=history)
     response = llm.call_llm()
     
-    return [
-        {"role": "assistant", "content": response}
-    ]
+    return {"role": "assistant", "content": response}
+
+  
 
 if __name__ == "__main__":
     gr.ChatInterface(fn=process_llm, type="messages").launch()
--- components/llm.py
+++ components/llm.py
@@ -1,19 +1,28 @@
 from groq import Groq
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 class LLM:
-    def __init__(self, query: str):
-        self.client = Groq(api_key = os.getenv('GROQ_URL'))
+    def __init__(self, query: str, history: list):
+        self.client = Groq(api_key=os.getenv('GROQ_URL'))
         self.model = "llama-3.3-70b-versatile"
         self.query = query  
-        
+        self.history = history 
+
     def call_llm(self):
+    # Ensure history follows Groq's message format
+        messages = [{"role": msg["role"], "content": msg["content"]} for msg in self.history]
+        
+        messages.append({"role": "user", "content": self.query})
+        
         response = self.client.chat.completions.create(
-            model = self.model,
-            messages = [{"role": "user", "content": self.query}],
-            max_tokens = 250,
-            temperature = 0.7
+            model=self.model,
+            messages=messages, 
+            max_tokens=250,
+            temperature=0.7
         )
+        
         return response.choices[0].message.content
+
