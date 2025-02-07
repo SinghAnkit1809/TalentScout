9b4122874ec519040a2a1380c2b0b86a49421e64
Ankit Singh <ankitsingh18092002@gmail.com>
2025-02-07 15:34:16 +0530

    updated llm function in ui

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import gradio as gr
 from components.llm import LLM
 
+llm= LLM()
 def process_llm(message, history):
-    history = history or []
-
-    llm = LLM(query=message, history=history)
-    response = llm.call_llm()
-    
-    return {"role": "assistant", "content": response}
-
-  
+    message =message.strip()
+    if message.lower() == "exit":
+        final_message = llm.finalize_interview()
+        history.append(message, final_message)
+        return "", history
+    else:
+        response = llm.conduct_interview(message)
+        history.append(message, response)
+        return "", history
 
 if __name__ == "__main__":
     gr.ChatInterface(fn=process_llm, type="messages").launch()
